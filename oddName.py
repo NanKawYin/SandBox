e8653775392235354bba7a0f291bd523cf77d454
NanKawYin <kawyin.nan@my.jcu.edu.au>
2019-08-04 18:08:01 +0800

    add name function

--- oddName.py
+++ oddName.py
@@ -1,1 +1,8 @@
-"""NankawYin"""
+"""NanKawYin"""
+
+name = str(input("Enter Your Name: "))
+name_count = len(name)
+while name_count == 0:
+    print("User name cannot be empty.")
+    name = input("Enter Your Name: ")
+print(name[0:name_count+1:2])
